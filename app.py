7125fc91fb5ed08e00180ae6bc527750a783cc5e
mr-cri-spy <kiraj8899@gmail.com>
2026-02-22 23:30:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 
 
+
 from dotenv import load_dotenv
 import os
 import streamlit as st
@@ -61,6 +62,7 @@ if st.button("Submit"):
     # Display Response
     st.markdown("<h3 style='color: white;'>Bot's Response:</h3>", unsafe_allow_html=True)
     st.write(response.text)
+
 
 
 
